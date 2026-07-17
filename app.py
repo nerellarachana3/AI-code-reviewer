2e252883803cfe7ec2dcf6fd068409c619b48066
nerellarachana3 <nerellarachana3@gmail.com>
2026-07-17 09:21:49 +0530

    Implement AI code review interface using Gradio

---
+++ app.py
@@ -0,0 +1,31 @@
+from google import genai
+import gradio as gr
+
+client=genai.Client(api_key="Gemini API")
+
+def review(code):
+  prompt=f"""
+  Review the code
+
+  Includes:
+  -Summary
+  -Bugs
+  -Code style
+  -Rating out of 10
+
+  code:
+  {code}
+  """
+  response=client.models.generate_content(
+      model="gemini-2.5-flash",
+      contents=prompt
+  )
+  return response.text
+demo=gr.Interface(
+    fn=review,
+    inputs=gr.Textbox(label="code"),
+    outputs="markdown",
+    title="AI code Review(gemini)"
+
+)
+demo.launch()
